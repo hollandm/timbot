ad9ec7e5465f0d4fed54abf80ca2790ae2e1a5ac
mbholland333@gmail.com <mbholland333@gmail.com@b5db5b55-0f59-6d7c-8311-7d7603cc1942>
2013-02-13 02:55:25 +0000

    Network Fail

--- trunk/timbot/Java/BotTopProgram/src/NetworkComms.java
+++ trunk/timbot/Java/BotTopProgram/src/NetworkComms.java
@@ -1,21 +1,74 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.SocketAddress;
+import java.net.UnknownHostException;
+
 
 public class NetworkComms implements Runnable {
 
 	float leftSpeed = 0;
 	float rightSpeed = 0;
-	
+
 	float armSpeed = 0;
 	float bucketSpeed = 0;
-	
+
 	int mode = 0;
-	
-	
+
+
+
+
 	@Override
 	public void run() {
+
+
+
+	}
+
+	public static void main(String args[]) throws IOException, InterruptedException {
+
+		
+		/*
+		byte[] dataIN = new byte[7];
+		
+		MulticastSocket in = new MulticastSocket(6000);
+		DatagramPacket recv = new DatagramPacket(dataIN, 7);
+		
+		in.receive(recv);
+		
+		System.out.println("Recieved: "+ dataIN[1]);
+		*/
+		
+		//client
+		byte[] data = new byte[7];
+		data[0]=0;
+		data[1]=0;
+		data[2]=0;
+		data[3]=0;
+		data[4]=0;
+		data[5]=0;
+		data[6]=0;
+		
+		
+//		DatagramSocket DGSocket = new DatagramSocket(6000);
+		MulticastSocket out = new MulticastSocket(6000);
+		InetAddress dest = InetAddress.getByName("224.0.0.1");
+//		InetAddress dest = InetAddress.getByName("255.255.255.255");
+//		InetAddress dest = InetAddress.getByName("192.168.0.119");
+		
+		DatagramPacket send = new DatagramPacket(data, 7, dest, 6000);
+		
+		while (true) {
+//			DGSocket.send(send);
+			out.send(send);
+			Thread.sleep(10);
+		}
 		
 		
 		
 	}
-	
-	
+
+
 }
