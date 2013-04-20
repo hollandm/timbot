c38edc194c530ef5acb822a16436870651d57da0
mbholland333@gmail.com <mbholland333@gmail.com@b5db5b55-0f59-6d7c-8311-7d7603cc1942>
2013-04-20 20:20:46 +0000

    Able to take picture, detect a blob of color with a good deal of accuracy. Draws image line on image where it thinks it found the image.

--- timbot/Java/JavaCV/src/multiCameraTest.java
+++ timbot/Java/JavaCV/src/multiCameraTest.java
@@ -1,12 +1,19 @@
-package snippet;
 
 import static com.googlecode.javacv.cpp.opencv_core.cvInRangeS;
 import static com.googlecode.javacv.cpp.opencv_core.cvScalar;
 import static com.googlecode.javacv.cpp.opencv_highgui.cvSaveImage;
+import static com.googlecode.javacv.cpp.opencv_core.cvCreateImage;
+import static com.googlecode.javacv.cpp.opencv_core.cvGetSize;
+import static com.googlecode.javacv.cpp.opencv_core.cvGet2D;
+import static com.googlecode.javacv.cpp.opencv_core.cvSet2D;
+import static com.googlecode.javacv.cpp.opencv_core.cvRectangle;
+import static com.googlecode.javacv.cpp.opencv_core.cvPoint;
+import java.nio.IntBuffer;
 
 import com.googlecode.javacv.FrameGrabber;
 import com.googlecode.javacv.FrameGrabber.Exception;
 import com.googlecode.javacv.VideoInputFrameGrabber;
+import com.googlecode.javacv.cpp.opencv_core.CvPoint;
 import com.googlecode.javacv.cpp.opencv_core.CvScalar;
 import com.googlecode.javacv.cpp.opencv_core.IplImage;
 import com.googlecode.javacv.cpp.opencv_highgui.CvCapture;
@@ -15,8 +22,14 @@ import com.googlecode.javacv.OpenCVFrameGrabber;
 
 public class multiCameraTest {
 
-	static CvScalar min = cvScalar(100, 100, 200, 0);//BGR-A
-    static CvScalar max= cvScalar(200, 200, 255, 0);//BGR-A
+//	Green
+//	static CvScalar min = cvScalar(0, 50, 0, 0);//BGR-A
+//    static CvScalar max= cvScalar(50, 200, 50, 0);//BGR-A
+
+//	Red
+	static CvScalar min = cvScalar(0, 0, 100, 0);//BGR-A
+    static CvScalar max= cvScalar(50, 50, 200, 0);//BGR-A
+    public static final int GRID_SIZE = 10; // size of the grid for searching for a color
 	
 	public static void main(String[] args) throws Exception {
 
@@ -26,7 +39,7 @@ public class multiCameraTest {
 		for(String string: cameras){
 			System.out.println(string);
 		}
-
+		int avgX[] = {0,0,0,0,0,0};
 		IplImage[] img = new IplImage[11];
 		IplImage[] thr = new IplImage[11];
 //		for (int i = 1; i < 5; ++i) {
@@ -54,21 +67,68 @@ public class multiCameraTest {
 			img[3] = cam3.grab();
 			img[4] = cam4.grab();
 			
-			//for (int i = 1; i < 5; i++) {
-			//	cvInRangeS(img[1], min, max, thr[1]);
-			//}
+			//TODO CHANGE The condition to 5
+			for (int i = 1; i < 5; i++) {
+				thr[i] = cvCreateImage(cvGetSize(img[i]),8,1);
+				cvInRangeS(img[i], min, max, thr[i]);
+				
+				int avgPix = 0;
+				int numPix = 0;
+						
+				//Scanning for colors
+				for (int r = 0; r < thr[i].height(); r+=GRID_SIZE) {
+					for (int c = r % GRID_SIZE; c < thr[i].width(); c+=GRID_SIZE) {
+						CvScalar p = cvGet2D(thr[i],r,c);
+						if (p.blue() == 255) {	//all colors should be 255 in this case
+							++numPix;
+							avgPix += c;
+						}
+//						System.out.println("b: "+p.blue() + " g: " + p.green() + " r: " + p.red());
+						
+					}
+				}
+				
+				//get real average value
+				avgPix = numPix ==0 ? avgPix : avgPix / numPix;
+				avgX[i] = avgPix;
+				System.out.println("Avg Position is " + avgPix );
+			}
 			
 			System.out.println(System.currentTimeMillis() - t0);
 
-			// my path
-			cvSaveImage("C:\\robots\\Webcam Pictures\\"+1+".jpg", img[1]);
-			cvSaveImage("C:\\robots\\Webcam Pictures\\"+2+".jpg", img[2]);
-			cvSaveImage("C:\\robots\\Webcam Pictures\\"+3+".jpg", img[3]);
-			cvSaveImage("C:\\robots\\Webcam Pictures\\"+4+".jpg", img[4]);
 			
+			
+			// my path
+//			cvSaveImage("C:\\robots\\Webcam Pictures\\"+1+".jpg", img[1]);
+//			cvSaveImage("C:\\robots\\Webcam Pictures\\"+2+".jpg", img[2]);
+//			cvSaveImage("C:\\robots\\Webcam Pictures\\"+3+".jpg", img[3]);
+//			cvSaveImage("C:\\robots\\Webcam Pictures\\"+4+".jpg", img[4]);
+//			
+			
+			// draw a line on the original image where we think the avg is
+			CvScalar linePix = new CvScalar();
+			linePix.setVal(0, 0);
+			linePix.setVal(1, 200);
+			linePix.setVal(2, 0);
+			CvPoint topPoint = cvPoint(avgX[1],0);
+			CvPoint botPoint = cvPoint(avgX[1], thr[1].height() -1);
+			
+			
+			cvRectangle(img[1],topPoint,botPoint,linePix,10,8,0);
+			//cvRectangle(thr[1],topPoint,botPoint,linePix,10,8,0);
+			cvSaveImage("C:\\robots\\Webcam Pictures\\"+1+".jpg", img[1]);
 			//cvSaveImage("C:\\robots\\Webcam Pictures\\"+1+"t.jpg", thr[1]);
-			//cvSaveImage("C:\\robots\\Webcam Pictures\\"+2+"t.jpg", thr[2]);
-			//cvSaveImage("C:\\robots\\Webcam Pictures\\"+3+"t.jpg", thr[3]);
+			//end: drawing lines
+			
+			
+			
+//			cvSaveImage("C:\\robots\\Webcam Pictures\\"+1+"t.jpg", thr[1]);
+//			cvSaveImage("C:\\robots\\Webcam Pictures\\"+2+"t.jpg", thr[2]);
+//			cvSaveImage("C:\\robots\\Webcam Pictures\\"+3+"t.jpg", thr[3]);
+//			cvSaveImage("C:\\robots\\Webcam Pictures\\"+4+"t.jpg", thr[4]);
+			
+
+			System.out.println("After Saving "+(System.currentTimeMillis() - t0));
 			
 			cam1.stop();
 			cam2.stop();
