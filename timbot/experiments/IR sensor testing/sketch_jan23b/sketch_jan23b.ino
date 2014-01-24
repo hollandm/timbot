ba266120837361fb2dd401c1be9c0c33bc2f896e
jman2000x@gmail.com <jman2000x@gmail.com@b5db5b55-0f59-6d7c-8311-7d7603cc1942>
2014-01-24 05:19:40 +0000

    updated

--- timbot/experiments/IR sensor testing/sketch_jan23b/sketch_jan23b.ino
+++ timbot/experiments/IR sensor testing/sketch_jan23b/sketch_jan23b.ino
@@ -1,4 +1,9 @@
-#define SAMPLE_AMOUNT 50
+/*
+* This program averages a number of samples together, designed to
+* read from an IR sensor.
+*/
+
+#define SAMPLE_AMOUNT 500
 
 int sensorPin = A0;
 int sensorValue = 0;
@@ -8,8 +13,8 @@ void setup() {
 };
 
 void loop() {
-  int sensorAverage = 0;
-  int sampleSum = 0;
+  long sensorAverage = 0;
+  long sampleSum = 0;
   for (int i = 0; i < SAMPLE_AMOUNT; i++) {
     // read the value(s) from the sensor:
     // won't need this technically, but it might be useful later.
