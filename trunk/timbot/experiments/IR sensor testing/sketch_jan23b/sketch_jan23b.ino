a56b7d6ee68c1d4ed6016d64151363a9756b99a3
jman2000x@gmail.com <jman2000x@gmail.com@b5db5b55-0f59-6d7c-8311-7d7603cc1942>
2014-01-24 05:15:52 +0000

    Added an IR sensor folder to the Experiments folder, contains a testing program and data tested from IR sensor #1. Please add any tested data to the excel document.

---
+++ trunk/timbot/experiments/IR sensor testing/sketch_jan23b/sketch_jan23b.ino
@@ -0,0 +1,22 @@
+#define SAMPLE_AMOUNT 50
+
+int sensorPin = A0;
+int sensorValue = 0;
+
+void setup() {
+  Serial.begin(9600);
+};
+
+void loop() {
+  int sensorAverage = 0;
+  int sampleSum = 0;
+  for (int i = 0; i < SAMPLE_AMOUNT; i++) {
+    // read the value(s) from the sensor:
+    // won't need this technically, but it might be useful later.
+    sensorValue = analogRead(sensorPin); 
+    sampleSum += sensorValue;
+    sensorAverage = sampleSum/SAMPLE_AMOUNT;
+  };
+  Serial.println(sensorAverage);
+  delay(50);
+};
