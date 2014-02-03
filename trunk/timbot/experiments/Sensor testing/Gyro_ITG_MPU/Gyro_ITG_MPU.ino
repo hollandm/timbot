65f1bc486bfe69752c06f6143d8faa9281fb6c7d
jman2000x@gmail.com <jman2000x@gmail.com@b5db5b55-0f59-6d7c-8311-7d7603cc1942>
2014-02-03 07:00:24 +0000

    Sensor testing updated sketches

---
+++ trunk/timbot/experiments/Sensor testing/Gyro_ITG_MPU/Gyro_ITG_MPU.ino
@@ -0,0 +1,97 @@
+/*
+* The MPU-6050 is a combination sensor including temp, gyro and accel.
+ * http://invensense.com/mems/gyro/documents/RM-MPU-6000A.pdf
+ * 
+ * Author: Jaimiey Sears
+ */
+
+#include <Wire.h>
+
+//device Hex codes to retrieve various values
+#define SLEEP 0x6B
+#define ACCEL_X_H  0x3B
+#define ACCEL_X_L  0x3C
+#define ACCEL_Y_H  0x3D
+#define ACCEL_Y_L  0x3E
+#define ACCEL_Z_H  0x3F
+#define ACCEL_Z_L  0x40
+
+//define different things that can go wrong
+//(for debugging purposes)
+#define SUCCESS              0
+#define MPU_DETECTION_ERROR  -1
+#define MPU_READ_ERROR       -2
+#define MPU_WRITE_ERROR      -3
+
+int deviceI2CAddress;
+
+void setup(){
+  Wire.begin();  //set up I2C bus
+  Serial.begin(9600); //set up to recieve data
+
+  //scan for an available device
+  Serial.println("Scanning for an available device...");
+
+  //try to find at device
+  int addressFound;
+  while (addressFound != 0){
+    addressFound = scanForI2C(&deviceI2CAddress);
+    if(addressFound == -1) {
+      Serial.println("Device not found, trying again...");
+    }
+  }
+  //print the good news once a device is found
+  Serial.print("Success! Device found at\t0x");
+  Serial.println(deviceI2CAddress,HEX);
+  Serial.println("Done scanning.");
+  Serial.println();
+
+  //wake the sensor
+}
+
+void loop(){
+  int error;
+  //Serial.println("Requesting data...");
+
+  byte accelX, accelY, accelZ;
+  
+  if (MPUread(ACCEL_X_H,1,&accelX) != SUCCESS){
+    Serial.println("Read Error");
+  }
+  if (MPUread(ACCEL_Y_H,1,&accelY) != SUCCESS){
+    Serial.println("Read Error");
+  }
+  if (MPUread(ACCEL_Z_H,1,&accelZ) != SUCCESS){
+    Serial.println("Read Error");
+  }
+  else{
+    Serial.print("Accelleration (x y z):\t");
+    Serial.print(accelX,DEC);
+    Serial.print("\t");
+    Serial.print(accelY,DEC);
+    Serial.print("\t");
+    Serial.println(accelZ,DEC);
+  }
+  delay(50);
+}
+
+/*
+* The function looks for a device
+ * @param  a the address of the variable to write the device address to.
+ * @return  0 success
+ * @return  -1 failure
+ */
+int scanForI2C(int * a){
+  byte error;
+  for(byte addr = 1; addr < 127; addr++){
+    Wire.beginTransmission(addr);
+    error = Wire.endTransmission();
+    if(error == 0){
+      *a = (int)addr; //write the address found to the global var
+      return SUCCESS;
+    }
+  }
+  //if no devices found, return the error
+  return MPU_DETECTION_ERROR;
+}
+
