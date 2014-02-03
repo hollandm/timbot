be7cef85262ad2f0c3b2ff677a06cd99f29e9068
mbholland333@gmail.com <mbholland333@gmail.com@b5db5b55-0f59-6d7c-8311-7d7603cc1942>
2014-02-03 02:37:53 +0000

    Added ir becon experiment

--- timbot/experiments/IR sensor testing/sketch_jan23b/sketch_jan23b.ino
+++ timbot/experiments/IR sensor testing/sketch_jan23b/sketch_jan23b.ino
@@ -6,23 +6,31 @@
 //the amount of times to sample per loop
 #define SAMPLE_AMOUNT 500
 
-int sensorPin = A0;
-int sensorValue = 0;
+int sensorPinL = A0;
+int sensorPinR = A1;
+int sensorValueL = 0;
+int sensorValueR = 0;
 
 void setup() {
   Serial.begin(9600);
 };
 
 void loop() {
-  long sensorAverage = 0;
-  long sampleSum = 0;
+  long sensorAverageL = 0;
+  long sensorAverageR = 0;
+  long sampleSumL = 0;
+  long sampleSumR = 0;
   for (int i = 0; i < SAMPLE_AMOUNT; i++) {
     // read the value(s) from the sensor:
     // won't need this technically, but it might be useful later.
-    sensorValue = analogRead(sensorPin); 
-    sampleSum += sensorValue;
-    sensorAverage = sampleSum/SAMPLE_AMOUNT;
+    sensorValueL = analogRead(sensorPinL); 
+    sensorValueR = analogRead(sensorPinR); 
+    sampleSumL += sensorValueL;
+    sampleSumR += sensorValueR;
+    sensorAverageL = sampleSumL/SAMPLE_AMOUNT;
+    sensorAverageR = sampleSumR/SAMPLE_AMOUNT;
   };
-  Serial.println(sensorAverage);
+  
+  Serial.println(max(sensorAverageL,sensorAverageR));
   delay(50);
 };
