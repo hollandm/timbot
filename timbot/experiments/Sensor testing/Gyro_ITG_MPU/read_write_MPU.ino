860a278e37ac7d88fe30efbf3edadeda28f63070
jman2000x@gmail.com <jman2000x@gmail.com@b5db5b55-0f59-6d7c-8311-7d7603cc1942>
2014-02-03 07:00:24 +0000

    Sensor testing updated sketches

---
+++ timbot/experiments/Sensor testing/Gyro_ITG_MPU/read_write_MPU.ino
@@ -0,0 +1,27 @@
+int MPUread(int addr, int numBytes, byte * data){
+  int n;
+  
+  Wire.beginTransmission(deviceI2CAddress);
+  if (Wire.write(addr) != 1){
+    return MPU_READ_ERROR;
+  }
+  if (Wire.endTransmission(false) != 0){ //hold the I2C-bus
+    return MPU_DETECTION_ERROR;
+  }
+  
+  Wire.requestFrom(deviceI2CAddress,numBytes,true); //release bus
+  
+  //get data
+  int i = 0;
+  while(Wire.available()){
+    data[i] = Wire.read();
+    i++;
+  }
+  if(i != numBytes){
+    return MPU_READ_ERROR;
+  }
+}
+
+int MPUwrite(int wake, int numBytes, byte * d){
+  
+}
