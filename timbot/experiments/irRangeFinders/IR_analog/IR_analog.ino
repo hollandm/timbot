8e15a7592b60a60ca40cc30dda767456a477d2cf
jman2000x@gmail.com <jman2000x@gmail.com@b5db5b55-0f59-6d7c-8311-7d7603cc1942>
2014-03-07 05:16:11 +0000

    Updated IR_analog.ino to measure 4 sensors at the same time. Took 4 data readings at once, and added them to IRdata.xlsx

--- timbot/experiments/irRangeFinders/IR_analog/IR_analog.ino
+++ timbot/experiments/irRangeFinders/IR_analog/IR_analog.ino
@@ -1,27 +1,80 @@
 /*
 * This program averages a number of samples together, designed to
-* read from an IR sensor.
-*/
+ * read from an IR sensor.
+ */
 
-#define SAMPLE_AMOUNT 500
+#define SAMPLE_AMOUNT 250
 
-int sensorPin = A0;
-long sensorValue = 0;
+int sensorPinF = A0;
+int sensorPinB = A1;
+int sensorPinR = A2;
+int sensorPinL = A3;
+
+typedef struct dataStore{
+  int D0;
+  int D1;
+  int D2;
+  int D3;
+
+  long sum0;
+  long sum1;
+  long sum2;
+  long sum3;
+
+  int avg0;
+  int avg1;
+  int avg2;
+  int avg3;
+};
 
 void setup() {
   Serial.begin(9600);
+  pinMode(sensorPinF,INPUT);
+  pinMode(sensorPinB,INPUT);
+  pinMode(sensorPinR,INPUT);
+  pinMode(sensorPinL,INPUT);
+
 };
 
 void loop() {
+  struct dataStore data;
+  data.sum0 =0;
+  data.sum1 = 0;
+  data.sum2 = 0;
+  data.sum3 = 0;
   long sensorAverage = 0;
   long sampleSum = 0;
   for (int i = 0; i < SAMPLE_AMOUNT; i++) {
+
+
     // read the value(s) from the sensor:
-    // won't need this technically, but it might be useful later.
-    sensorValue = analogRead(sensorPin); 
-    sampleSum += sensorValue;
-    sensorAverage = sampleSum/SAMPLE_AMOUNT;
+    data.D0 = analogRead(sensorPinF);
+    data.D1 = analogRead(sensorPinB);
+    data.D2 = analogRead(sensorPinR);
+    data.D3 = analogRead(sensorPinL);
+    data.sum0 += data.D0;
+    data.sum1 += data.D1;
+    data.sum2 += data.D2;
+    data.sum3 += data.D3;
   };
-  Serial.println(sensorAverage);
-  delay(50);
+
+  data.avg0 = data.sum0/SAMPLE_AMOUNT;
+  data.avg1 = data.sum1/SAMPLE_AMOUNT;
+  data.avg2 = data.sum2/SAMPLE_AMOUNT;
+  data.avg3 = data.sum3/SAMPLE_AMOUNT;
+
+  Serial.print(data.avg0);
+  Serial.print("\t");
+  Serial.print(data.avg1);
+  Serial.print("\t");
+  Serial.print(data.avg2);
+  Serial.print("\t");
+  Serial.print(data.avg3);
+  Serial.println();
+
 };
+
+
+
+
+
